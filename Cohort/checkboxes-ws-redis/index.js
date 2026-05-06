384a44428169d59f073f8e6e755bd3ee28ced1f2
vibhu <pariseyshubhang@gmail.com>
2026-05-06 21:13:02 +0530

    added redis from common ws (maintaing state from diff sePORTS ) also implemented rate limiting

--- Cohort/checkboxes-ws-redis/index.js
+++ Cohort/checkboxes-ws-redis/index.js
@@ -2,9 +2,16 @@ import http from 'node:http'
 import path from 'node:path'
 import { Server } from 'socket.io'
 import express from 'express'
+import { publisher,subscriber,redis } from './redis-connection.js'
+
 
 
 const CHECKBOX_SIZE = 100
+
+const CHECKBOX_KEY = 'checkbox-state:v1'
+
+const rateLimitingHashMap =new Map()
+
 const state = {
     CHECKBOXES : new Array(CHECKBOX_SIZE).fill(false)
 }
@@ -18,18 +25,50 @@ async function main() {
     const io = new Server()
 
     io.attach(server)
-
+    await subscriber.subscribe('internal-server:checkbox:change')
+    subscriber.on('message',(channel, message)=>{
+        if(channel === 'internal-server:checkbox:change'){
+            const {index, checked} = JSON.parse(message)
+            io.emit('server:checkbox-change',{index,checked})
+        }
+    })
     // static files
 
     //socket handler
     io.on("connection",(socket)=>{
         console.log(`Socket connected `,{id:socket.id})
         //handle the (client:checkbox-change) here
-        socket.on(`client:checkbox-change`,(data)=>{
+        socket.on(`client:checkbox-change`,async(data)=>{
             console.log(`[Socket:${socket.id}],client:checkbox-change`,data)
+
+            const lastOperation = await redis.get(`rate-limiting:${socket.id}`)
+            if(lastOperation){
+                const timeElapsed = Date.now() - lastOperation
+                if(timeElapsed < 5 * 1000){
+                    socket.emit('server:error',{error : 'Rate limited: wait 5 seconds between changes'})
+                    return
+                }
+            }
+
+            await redis.set(`rate-limiting:${socket.id}`,Date.now())
+
             const {index, checked} = data
-            state.CHECKBOXES[index] = checked
-            io.emit('client:checkbox-change',data)
+            const existingState =await redis.get(CHECKBOX_KEY)
+            if(existingState){
+                const remotedata = JSON.parse(existingState)
+                remotedata[index] = data.checked
+                redis.set(CHECKBOX_KEY, JSON.stringify(remotedata))
+            } else {
+                redis.set(CHECKBOX_KEY,JSON.stringify(new Array(CHECKBOX_SIZE).fill(false)))
+            }
+            
+
+            // state.CHECKBOXES[index] = checked
+            // io.emit('server:checkbox-change',data)
+            //instead of telling to all i ll say and update to my redis server
+            //it will tell to all and update state
+            await publisher.publish('internal-server:checkbox:change',JSON.stringify(data))
+
         })
     })
 
@@ -42,8 +81,13 @@ async function main() {
         res.json({ healthy: true })
     )
 
-    app.get('/checkboxes',(req,res)=>{
-        return res.json({checkboxes:state.CHECKBOXES})
+    app.get('/checkboxes',async(req,res)=>{
+        const existingState =await redis.get(CHECKBOX_KEY)
+            if(existingState){
+                const remotedata = JSON.parse(existingState)
+                return res.json({checkboxes:remotedata})
+            }
+            return res.json({checkboxes:new Array(CHECKBOX_SIZE).fill(false)})
     })
 
     server.listen(PORT, () => {
