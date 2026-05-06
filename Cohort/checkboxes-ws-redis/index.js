64119f3ddf6418c6828b8ef8a46e672c0a9def1c
vibhu <pariseyshubhang@gmail.com>
2026-05-06 13:18:13 +0530

    basic running ws

---
+++ Cohort/checkboxes-ws-redis/index.js
@@ -0,0 +1,43 @@
+import http from 'node:http'
+import path from 'node:path'
+import { Server } from 'socket.io'
+import express from 'express'
+
+
+async function main() {
+
+    const app = express()
+    const server = http.createServer(app)
+    const PORT = process.env.PORT ?? 8080
+    const io = new Server()
+
+    io.attach(server)
+
+    // static files
+
+    //socket handler
+    io.on("connection",(socket)=>{
+        console.log(`Socket connected `,{id:socket.id})
+        //handle the (client:checkbox-change) here
+        socket.on(`client:checkbox-change`,(data)=>{
+            console.log(`[Socket:${socket.id}],client:checkbox-change`,data)
+            io.emit('client:checkbox-change',data)
+        })
+    })
+
+
+    //express handler
+    
+    app.use(express.static(path.resolve('./public')))
+
+    app.get('/health', (req, res) =>
+        res.json({ healthy: true })
+    )
+
+
+    server.listen(PORT, () => {
+        console.log(`Server listening on http://localhost:${PORT}`)
+    })
+}
+
+main()
